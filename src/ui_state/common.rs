563f3bc6e9664dbca678dd1038784b9459969e1d
Domtron Vox <domtron.vox@gmail.com>
2021-12-15 01:52:42 -0500

    Fixed issue with camera zooming. Fixed grid being drawn along zx instead of xy plane. Moved camera code to common.rs so other ui states can use it.

--- src/ui_state/common.rs
+++ src/ui_state/common.rs
@@ -2,8 +2,12 @@ use super::{StateEvent, UIStateMachine,
     StationViewState, OrbitalViewState, PlanetViewState, MissionViewState};
 
 use macroquad::{
-    math::Vec2,
+    math::{vec2, vec3, Vec2, Vec3, Quat},
     ui::{hash, Ui, widgets},
+    input::{is_key_down, KeyCode},
+    models::{draw_line_3d},
+    camera::Camera3D,
+    color::Color,
 };
 
 
@@ -11,7 +15,7 @@ use macroquad::{
 
 
 
-
+//UI stuff for the common view_switcher panel
 pub fn view_switcher(current_tab: u32, state_machine: &mut UIStateMachine, 
         ui: &mut Ui, size: Vec2) {
 
@@ -37,4 +41,107 @@ pub fn view_switcher(current_tab: u32, state_machine: &mut UIStateMachine,
             
         }
     );
+}
+
+
+/// Draw a grid centered at (0, 0, 0) code from macroquad to fix grid orientation
+pub fn draw_grid(slices: u32, spacing: f32, axes_color: Color, other_color: Color) {
+    let half_slices = (slices as i32) / 2;
+    for i in -half_slices..half_slices + 1 {
+        let color = if i == 0 { axes_color } else { other_color };
+
+        draw_line_3d(
+            vec3(i as f32 * spacing, -half_slices as f32 * spacing, 0.),
+            vec3(i as f32 * spacing, half_slices as f32 * spacing, 0.),
+            color,
+        );
+        draw_line_3d(
+            vec3(-half_slices as f32 * spacing, i as f32 * spacing, 0.),
+            vec3(half_slices as f32 * spacing, i as f32 * spacing, 0.),
+            color,
+        );
+    }
+}
+
+
+
+pub fn handle_camera_controls(camera: &mut Camera3D) {
+    //handle camera movement controls
+    let mut dir = (0., 0.);
+    let mut rot_speed = 0.01;
+    let mut zoom_speed = 0.;
+    
+    //figure out direction of movement
+    if is_key_down(KeyCode::A) || is_key_down(KeyCode::Left) {
+        dir.0 -= 1.;
+    }
+    if is_key_down(KeyCode::D) || is_key_down(KeyCode::Right) {
+        dir.0 += 1.;
+    }
+    if is_key_down(KeyCode::W) || is_key_down(KeyCode::Up) {
+        dir.1 -= 1.;
+    }
+    if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
+        dir.1 += 1.;
+    }
+    
+    let is_shift = is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift);
+    
+    //figure out direction of zoom
+    if is_key_down(KeyCode::Q) {
+        if ! is_shift {
+            zoom_speed += 0.99;
+        } else {
+            zoom_speed += 0.90;
+        }            
+    }
+    if is_key_down(KeyCode::E) {
+        if ! is_shift {
+            zoom_speed += 1.01;
+        } else {
+            zoom_speed += 1.10;
+        }
+    }
+    
+    //toggle fast rotation
+    if is_shift {
+        rot_speed += 0.10;
+    }
+    
+    
+    //handle calculating new camera position when player trys to move camera.
+    if dir != (0., 0.) {
+        //We need to calculate rotating the camera around the target
+        
+        //Adjust camera position as if it was rotating around origin.
+        let adjusted_pos = camera.position - camera.target;
+        
+        //Left-Right rotates us around the z axis
+        let z_rotation = Quat::from_rotation_z( dir.0 * rot_speed );
+        
+        //Up-Down rotates us around an axis perpendicular to current view and Z axis
+        let axis = Vec3::Z.cross( adjusted_pos.normalize() ).normalize();
+        let xy_rotation = Quat::from_axis_angle( axis, (dir.1 * rot_speed) );
+        
+        //TODO need to limit xy rotation
+
+        //merge the two rotations
+        let rotation = z_rotation.mul_quat(xy_rotation);
+        
+        //rotate our adjusted position then readjust back to the target relative origin
+        camera.position = rotation.mul_vec3(adjusted_pos) + camera.target;
+    }
+    
+    //handle zoom if player trys to zoom
+    if zoom_speed != 0. {
+        //zoom in
+        if zoom_speed < 1. && camera.position.length() > 2. {
+            camera.position = camera.position * zoom_speed;
+        }
+        
+        //zoom out
+        if zoom_speed > 1. && camera.position.length() < 30. {
+            camera.position = camera.position * zoom_speed;
+        }
+    }
 }
--- src/ui_state/orbital_view_state.rs
+++ src/ui_state/orbital_view_state.rs
@@ -1,4 +1,4 @@
-use super::{UIState, StateEvent, UIStateMachine, view_switcher};
+use super::{UIState, StateEvent, UIStateMachine, view_switcher, draw_grid, handle_camera_controls};
 use crate::Simulation;
 
 
@@ -9,7 +9,6 @@ use macroquad::{
     ui::{hash, root_ui, widgets},
     input::{is_key_down, KeyCode},
     camera::{Camera3D, set_camera},
-    models::draw_grid,
     color::colors::WHITE,
 };
 
@@ -24,8 +23,8 @@ impl OrbitalViewState {
     pub fn new() -> OrbitalViewState {
         OrbitalViewState {
             camera: Camera3D{
-                target: vec3(10., 0., 0.),
-                position: vec3(5., 0., 0.),
+                target: vec3(0., 0., 0.),
+                position: vec3(5., 5., 5.),
                 ..Default::default()
             },
         }
@@ -35,68 +34,9 @@ impl OrbitalViewState {
 impl UIState for OrbitalViewState {
 
     fn world_render(&mut self, state_machine: &mut UIStateMachine, sim: &mut Simulation) {
-        //handle camera movement controls
-        let mut dir = (0., 0.);
-        let mut rot_speed = 0.01;
-        let mut zoom_speed = 0.;
         
-        //figure out direction of movement
-        if is_key_down(KeyCode::A) || is_key_down(KeyCode::Left) {
-            dir.0 -= 1.;
-        }
-        if is_key_down(KeyCode::D) || is_key_down(KeyCode::Right) {
-            dir.0 += 1.;
-        }
-        if is_key_down(KeyCode::W) || is_key_down(KeyCode::Up) {
-            dir.1 -= 1.;
-        }
-        if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
-            dir.1 += 1.;
-        }
-        
-        //figure out direction of zoom
-        if is_key_down(KeyCode::Q) {
-            zoom_speed += 0.99;
-        }
-        if is_key_down(KeyCode::E) {
-            zoom_speed -= 0.99;
-        }
-        
-        //toggle fast rotation
-        if is_key_down(KeyCode::LeftShift) || is_key_down(KeyCode::RightShift) {
-            rot_speed += 0.04;
-            zoom_speed *= 2.;
-        }
-        
-        
-        //handle calculating new camera position when player trys to move camera.
-        if dir != (0., 0.) {
-            //We need to calculate rotating the camera around the target
-            
-            //Adjust camera position as if it was rotating around origin.
-            let adjusted_pos = self.camera.position - self.camera.target;
-            
-            //Left-Right rotates us around the z axis
-            let z_rotation = Quat::from_rotation_z( dir.0 * rot_speed );
-            
-            //Up-Down rotates us around an axis perpendicular to current view and Z axis
-            let axis = Vec3::Z.cross( adjusted_pos.normalize() );
-            let xy_rotation = Quat::from_axis_angle( axis, (dir.1 * rot_speed) );
-            
-            //TODO need to limmit xy roa
-
-            //merge the two rotations
-            let rotation = z_rotation.mul_quat(xy_rotation);
-            
-            //rotate our adjusted position then readjust back to the target relative origin
-            self.camera.position = rotation.mul_vec3(adjusted_pos) + self.camera.target;
-        }
-        
-        //handle zoom if player trys to zoom
-        if zoom_speed > 0. {
-            //can just multiply position by a scaler
-            //self.camera.position *= zoom_speed;
-        }
+        //check for camera control input and update camera accordingly
+        handle_camera_controls(&mut self.camera);
         
         //set active camera
         set_camera(&self.camera);
